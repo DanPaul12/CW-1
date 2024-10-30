4223fbddf12eb97c05f008a8873994cc89fe583c
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-30 11:32:47 -0700

    cw1

--- newapp.py
+++ newapp.py
@@ -19,8 +19,38 @@ ma = Marshmallow(app)
 db = SQLAlchemy(app, model_class=Base)
 CORS(app)
 
+#----------------------------------------------------------------------------
+
+class Customer(db.Model):
+    __tablename__ = 'Customer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String (255), nullable = False)
+    email = db.Column(db.String (320))
+    phone = db.Column(db.String (15))
+
+class CustomerSchema(ma.Schema):
+    name = fields.String(required=True)
+    email = fields.String(required=True)
+    phone = fields.String(required=True)
+
+customer_schema = CustomerSchema()
 
 
+#-------------------------------------------------------------------------------
+
+@app.route('customers', methods=['POST']) 
+def add_customer():
+    try:
+        cusomer_data = customer_schema.load(request.json)
+        customer = Customer(name = cusomer_data['name'], email = cusomer_data['email'], phone = cusomer_data['phone'])
+        db.session.add(customer)
+        db.session.commit()
+        return jsonify({'message': 'customer added'}), 200
+    except ValidationError as error:
+        return jsonify(error.messages), 404
 
 
+#--------------------------------------------------------------------------------------
 
+if __name__ == "__main__":
+    app.run(debug=True)
