743305fb5012c8204c922b024b0d5dd58869910a
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-29 22:34:07 -0700

    cw1-1

--- newapp.py
+++ newapp.py
@@ -1,5 +1,17 @@
 import datetime
 from typing import List
-from flask import Flask 
+from flask import Flask, jsonify, request
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, session 
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select, delete
 from marshmallow import fields, validate, ValidationError
+
+
+app = Flask(__name__)
+ma = Marshmallow(app)
+app.config
+
+class Base(DeclarativeBase):
+    pass
