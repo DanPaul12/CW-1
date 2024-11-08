21456a9b698491b94c74272c5aae5fcf3f207045
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-08 08:23:03 -0800

    cw1

--- flask-ecommerce-ui/src/components/productForm.jsx
+++ flask-ecommerce-ui/src/components/productForm.jsx
@@ -1,20 +1,31 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
-const ProductForm = (selectedProduct) => {        //whats deal with semicolons and inputting prop types?
-    const nameRef = useRef(null)                  //what exactly does {} represent in jsx
-    const priceRef = useRef(null)
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const name = nameRef.current.value 
-        const price = priceRef.current.value 
-        console.log("name:", {name}, "price:", {price})
+const ProductForm = (selectedProduct, onUpdate) => { 
+    const [name, setName] = useState('') 
+    const [price, setPrice] = useState('')  
+    
+    useEffect(()=>{
         if (selectedProduct){
-            const editProduct = async () => 
-                axios.put('/products{}')
-        } else {
-            axios.post('/products')
+        setName(selectedProduct.name)
+        setPrice(selectedProduct.price)}
+    },[selectedProduct])
+    
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const productData = {name, price}                     //whats happening here
+        console.log("name:", {name}, "price:", {price})
+        try{
+            if (selectedProduct){
+                await axios.put(`http://127.0.0.1:5000/products${selectedProduct.id}`, productData)
+            } else {
+                await axios.post('http://127.0.0.1:5000/products', productData)
+            }
+            setName('')
+            setPrice('')
+        }catch (error){
+            console.error('Error:', error)
         }
     }
 
@@ -23,12 +34,12 @@ const ProductForm = (selectedProduct) => {        //whats deal with semicolons a
             <h3>Product Form</h3>
             <label>
                 Name
-                <input type="text" ref={nameRef}></input>
+                <input type="text" value={name} onChange={(e)=> setName(e.target.value)}></input>
             </label>
             <br />
             <label>
                 Price
-                <input type="text" ref={priceRef}></input>
+                <input type="text" value={price} onChange={(e)=> setPrice(e.target.value)}></input>
             </label>
             <br />
             <button type="submit">Submit</button>
